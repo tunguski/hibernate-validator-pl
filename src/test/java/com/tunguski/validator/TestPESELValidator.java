044028eac122549acc4252e1bdfc8d806805df68
Marek Romanowski <marek.romanowski@gmail.com>
2014-07-04 14:54:56 +0200

    Automatic coveralls reports

---
+++ src/test/java/com/tunguski/validator/TestPESELValidator.java
@@ -0,0 +1,13 @@
+package com.tunguski.validator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TestPESELValidator {
+
+  @Test
+  public void testIsValid() throws Exception {
+    PESELValidator validator = new PESELValidator();
+  }
+}
