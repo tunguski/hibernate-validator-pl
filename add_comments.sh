4cf22f115211b2be0c9573a0260c138a7c787e58
Marek Romanowski <marek.romanowski@gmail.com>
2015-10-02 22:58:11 +0200

    Extend script

--- add_comments.sh
+++ add_comments.sh
@@ -7,13 +7,21 @@ HOST=http://gitlab.matsuo.pl
 PROJECT_ID=$(curl -H "PRIVATE-TOKEN: $USER_PRIVATE_TOKEN" $HOST/ci/api/v1/projects | python -m json.tool | grep -E "gitlab_id|\"id\"" | grep "id\": $CI_PROJECT_ID," -B 1 | grep "\"gitlab_id\"" | cut -d ":" -f 2 | cut -d "," -f 1 | sed -e 's/^[[:space:]]*//')
 COMMIT_ID=$CI_BUILD_REF
 
-
+CHECKSTYLE=$(cat target/checkstyle-result.xml  | grep "<error " | wc -l)
+PMD=$(cat target/pmd.xml | grep "<violation" | wc -l)
+FIND_BUGS=$(cat target/findbugsXml.xml | sed 's/total_bugs/\ntotal_bugs/g' | cut -f 1 -d " " | grep total_bugs | cut -f 2 -d "'" | head -n 1)
 
 
 URL=$HOST/api/v3/projects/$PROJECT_ID/repository/commits/$COMMIT_ID/comments
 
 echo $URL
 
-curl --data $'note=This is the content of comment' \
+NOTE=$"# Build result"
+NOTE=$"$NOTE\n| Type | Result |"
+NOTE=$"$NOTE\n| Checkstyle | $CHECKSTYLE |"
+NOTE=$"$NOTE\n| Pmd | $PMD |"
+NOTE=$"$NOTE\n| Find Bugs | $FIND_BUGS |"
+
+curl --data $"note=$NOTE" \
     -H "PRIVATE-TOKEN: $USER_PRIVATE_TOKEN" \
     $URL 
