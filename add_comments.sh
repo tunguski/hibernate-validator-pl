2b2c923f5b8da18289a5273c6439fc9a5983b67c
Marek Romanowski <marek.romanowski@gmail.com>
2015-10-02 22:50:12 +0200

    Fix script

--- add_comments.sh
+++ add_comments.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-PROJECT_ID=17
-COMMIT_ID=60d80027
-URL=http://gitlab.matsuo.pl/api/v3/projects/$PROJECT_ID/repository/commits/$COMMIT_ID/comments
+# CI_PROJECT_ID
+
+HOST=http://gitlab.matsuo.pl
+
+PROJECT_ID=$(curl -H "PRIVATE-TOKEN: $USER_PRIVATE_TOKEN" $HOST/ci/api/v1/projects | python -m json.tool | grep -E "gitlab_id|\"id\"" | grep "id\": $CI_PROJECT_ID," -B 1 | grep "\"gitlab_id\"" | cut -d ":" -f 2 | cut -d "," -f 1 | sed -e 's/^[[:space:]]*//')
+COMMIT_ID=$CI_BUILD_REF
+
+
+
+
+URL=$HOST/api/v3/projects/$PROJECT_ID/repository/commits/$COMMIT_ID/comments
 
 echo $URL
 
