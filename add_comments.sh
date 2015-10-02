5256c74c430602288e457e3facb93f9bee209f90
Marek Romanowski <marek.romanowski@gmail.com>
2015-10-02 22:25:23 +0200

    Fix script

--- add_comments.sh
+++ add_comments.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-$PROJECT_ID=17
-$COMMIT_ID=60d80027
+PROJECT_ID=17
+COMMIT_ID=60d80027
+URL=http://gitlab.matsuo.pl/api/v3/projects/$PROJECT_ID/repository/commits/$COMMIT_ID/comments
+
+echo $URL
 
 curl --data $'note=This is the content of comment' \
     -H "PRIVATE-TOKEN: $USER_PRIVATE_TOKEN" \
-    http://gitlab.matsuo.pl/api/v3/projects/$PROJECT_ID/repository/commits/$COMMIT_ID/comments
+    $URL 
