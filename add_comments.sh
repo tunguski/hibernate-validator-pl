0ee781231b303b29677628819ff6ca251553d101
Marek Romanowski <marek.romanowski@gmail.com>
2015-10-02 22:22:15 +0200

    fix script

--- add_comments.sh
+++ add_comments.sh
@@ -5,4 +5,4 @@ $COMMIT_ID=60d80027
 
 curl --data $'note=This is the content of comment' \
     -H "PRIVATE-TOKEN: $USER_PRIVATE_TOKEN" \
-    http://gitlab.you.domain.com/api/v3/projects/$PROJECT_ID/repository/commits/$COMMIT_ID/comments
+    http://gitlab.matsuo.pl/api/v3/projects/$PROJECT_ID/repository/commits/$COMMIT_ID/comments
